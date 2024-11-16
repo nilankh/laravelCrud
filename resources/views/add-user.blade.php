2178203731e0e73910abe249a4b09821a104f527
nilank nikhil <nilanknikhil@gmail.com>
2024-11-16 16:38:37 +0530

    added validation on ui level

--- resources/views/add-user.blade.php
+++ resources/views/add-user.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New User</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
 <body>
     <div class="container">
@@ -15,22 +16,37 @@
                     <div class="mb-3">
                         <label for="formGroupExampleInput" class="form-label">Full Name</label>
                         <input type="text" name="full_name" class="form-control" id="formGroupExampleInput" placeholder="Enter Full Name">
+                        @error('full_name')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Email</label>
                         <input type="email" name="email" class="form-control" id="formGroupExampleInput2" placeholder="Enter Email">
+                        @error('email')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Phone Number</label>
                         <input type="text" name="phone_number" class="form-control" id="formGroupExampleInput2" placeholder="Enter Phone Number">
+                        @error('phone_number')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Password</label>
                         <input type="password" name="password" class="form-control" id="formGroupExampleInput2" placeholder="Enter Password">
+                        @error('password')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
                     </div>
                     <div class="mb-3">
                         <label for="formGroupExampleInput2" class="form-label">Confirm Password</label>
                         <input type="password" name="password_confirmation" class="form-control" id="formGroupExampleInput2" placeholder="Confirm Password">
+                        @error('password_confirmation')
+                            <span class="text-danger">{{$message}}</span>
+                        @enderror
                     </div>
                 <button type="submit" class="btn btn-primary">Save</button>
                 </form>
