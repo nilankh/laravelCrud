01095568fdf6124dcba2a361cd8e04885011859f
nilank nikhil <nilanknikhil@gmail.com>
2024-11-16 14:49:10 +0530

    Project set up done and created first api

---
+++ resources/views/users.blade.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hello Harbour Space</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+</head>
+<body>
+    {{-- here we will pass list of all users --}}
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Harbour Space Crud
+            </div>
+            <div class="card-body">
+                <table>
+                    <thread>
+                        <th>S/N</th>
+                        <th>Full Name</th>
+                        <th>Email</th>
+                        <th>Phone Number</th>
+                        <th>Registration Date</th>
+                        <th>Last Update</th>
+                    </thread>
+                    <tbody>
+                        
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
